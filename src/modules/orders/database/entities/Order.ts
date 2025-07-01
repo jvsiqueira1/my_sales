5b9c1eda62dc5d3f51ba1a89ccdef90f28239dbc
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-01 16:53:02 -0400

    Feat: entity orders_products

--- src/modules/orders/database/entities/Order.ts
+++ src/modules/orders/database/entities/Order.ts
@@ -1,5 +1,6 @@
 import { Customer } from "@modules/customers/database/entities/Customer";
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { OrdersProducts } from "./OrdersProducts";
 
 @Entity('orders')
 export class Order {
@@ -7,8 +8,13 @@ export class Order {
   id: number
 
   @ManyToOne(() => Customer)
-  @JoinColumn()
+  @JoinColumn({name: 'customer_id'})
   customer: Customer
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts
 
   @CreateDateColumn()
   created_at: Date
