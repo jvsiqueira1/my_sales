3bcc5d6354fd0328267f938c75e2353d8518de94
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-01 11:26:24 -0400

    Feat: auth middleware

--- src/modules/users/routes/UserRoutes.ts
+++ src/modules/users/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import UsersControllers from "../controllers/UsersControllers"
 import { createUserSchema } from "../schemas/UserSchemas"
+import AuthMiddleware from "@shared/middlewares/AuthMiddleware"
 
 const usersRouter = Router()
 const usersController = new UsersControllers()
 
-usersRouter.get('/', usersController.index)
+usersRouter.get('/', AuthMiddleware.execute, usersController.index)
 usersRouter.post('/', createUserSchema, usersController.create)
 
 export default usersRouter
