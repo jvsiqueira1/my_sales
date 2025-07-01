ba371c59ae5f89f8e7e54ea632d8588de95eefe6
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-01 11:11:02 -0400

    Feat: user session routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productsRouter from "@modules/products/routes/ProductRoute"
+import sessionsRouter from "@modules/users/routes/SessionRoutes"
 import usersRouter from "@modules/users/routes/UserRoutes"
 import { Router } from "express"
 
@@ -7,5 +8,6 @@ const routes = Router()
 routes.get('/health', (_request, response) => {return response.json({ message: 'Hello Dev! I am Alive!' })})
 routes.use('/products', productsRouter)
 routes.use('/users', usersRouter)
+routes.use('/sessions', sessionsRouter)
 
 export default routes
