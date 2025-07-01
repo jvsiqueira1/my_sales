198b90321eaa7d03fa82c65aec2b8e4753bcdc0b
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-01 00:15:28 -0400

    Feat: product controllers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,6 @@ import { Router } from "express"
 
 const routes = Router()
 
-routes.get('/health', (_request, response) => {
-  return response.json({ message: 'Hello Dev! I am Alive!' })
-})
+routes.get('/health', (_request, response) => {return response.json({ message: 'Hello Dev! I am Alive!' })})
 
 export default routes
