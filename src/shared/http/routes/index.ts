734dd953ce8c9fa98714f1074242e308801a4301
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-01 00:22:14 -0400

    Feat: products routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
+import productsRouter from "@modules/products/routes/ProductRoute"
 import { Router } from "express"
 
 const routes = Router()
 
 routes.get('/health', (_request, response) => {return response.json({ message: 'Hello Dev! I am Alive!' })})
+routes.use('/products', productsRouter)
 
 export default routes
