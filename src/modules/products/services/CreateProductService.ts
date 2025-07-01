b69212f6f12477f0f5b7edce4f6746441551dd31
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-06-30 23:54:20 -0400

    Feat: service show, list and create product

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError"
+import { Product } from "../database/entities/Product"
+import { productsRepositories } from "../database/repositories/ProductsRepositories"
+
+interface ICreateProduct {
+  name: string
+  price: number
+  quantity: number
+}
+
+export default class CreateProductService {
+  async execute({name, price, quantity}: ICreateProduct): Promise<Product> {
+    const productsExists = await productsRepositories.finByName(name)
+
+    if(productsExists) {
+      throw new AppError('There is already on product with this name', 409)
+    }
+
+    const product = productsRepositories.create({name, price, quantity})
+
+    await productsRepositories.save(product)
+
+    return product
+  }
+}
