b4bb9f006d8b85469926003bdb8c692c2783329c
João Vítor de Siqueira Campos <joaovitorsc@gmail.com>
2025-07-02 09:45:42 -0400

    Feat: customer pagination

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,9 +1,23 @@
-import { Customer } from "../database/entities/Customer";
+import { IPagination } from "@shared/interfaces/pagination.interface";
 import { customerRepositories } from "../database/repositories/CustomerRepositories";
+import { Customer } from "../database/entities/Customer";
 
 export default class ListCustomerService {
-  async execute(): Promise<Customer[]> {
-    const customers = customerRepositories.find()
-    return customers
+  async execute(page: number = 1, limit: number = 10): Promise<IPagination<Customer>> {
+    const [ data, total ] = await customerRepositories.findAndCount({
+      take: limit,
+      skip: (page - 1) * limit,
+    })
+    const totalPages = Math.ceil(total / limit)
+
+    return {
+      data,
+      total,
+      per_page: limit,
+      current_page: page,
+      total_pages: totalPages,
+      next_page: page < totalPages ? page + 1 : null,
+      prev_page: page > 1 ? page - 1 : null,
+    } as IPagination<Customer>
   }
 }
